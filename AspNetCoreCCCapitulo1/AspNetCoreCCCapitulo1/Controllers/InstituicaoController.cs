e7b261ae2b8c38666fc4cfdb4afa161aa568ce60
Christovam Munhoz <cmunhozprog02@hotmail.com>
2018-03-02 11:54:11 -0300

    Criaçao do InsttuicaoController

---
+++ AspNetCoreCCCapitulo1/AspNetCoreCCCapitulo1/Controllers/InstituicaoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreCCCapitulo1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreCCCapitulo1.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+            {
+            new Instituicao() { InstituicaoID = 1, Nome = "UniParaná", Endereco = "Paraná"},
+            new Instituicao() { InstituicaoID = 2, Nome = "UniSanta", Endereco = "Santa Catarina"},
+            new Instituicao() { InstituicaoID = 3, Nome = "UniSãoPaulo", Endereco = "São Paulo"},
+            new Instituicao() { InstituicaoID = 4, Nome = "UniSulgrandense", Endereco = "Rio Grande do Sul"},
+            new Instituicao() { InstituicaoID = 5, Nome = "UniCarioca", Endereco = "Rio de Janeiro"}
+            };
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(instituicoes);
+        }
+    }
+}
